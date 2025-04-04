c05e720138a955b6b4144dd82e211306bdb0a41f
SompheaReak <Thesonggaming@gmail.com>
2025-04-05 05:07:12 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,9 +1,9 @@
 const products = [
-  { id: 1, category: "Toy", name: "Ninjago", price: 1.25, image: "IMG_2459.jpeg" },
-  { id: 2, category: "Toy", name: "Toy Car", price: 2.50, image: "IMG_2460.jpeg" },
-  { id: 3, category: "Toy", name: "Robot", price: 3.75, image: "IMG_2461.jpeg" },
-  { id: 4, category: "Toy", name: "Puzzle", price: 4.00, image: "IMG_2462.jpeg" },
-  { id: 5, category: "Jewelry", name: "Teddy Bear Necklace", price: 5.50, image: "IMG_2463.jpeg" },
-  { id: 6, category: "Toy", name: "Dinosaur Toy", price: 6.75, image: "IMG_2464.jpeg" },
-  { id: 7, category: "Shirt", name: "Superhero T-Shirt", price: 7.99, image: "IMG_4997.jpeg" },
+  { id: 1, name: "Ninjago", price: 1.25, image: "IMG_2459.jpeg", category: "Jewelry" },
+  { id: 2, name: "Toy Car", price: 2.50, image: "IMG_2460.jpeg", category: "Toy" },
+  { id: 3, name: "Robot", price: 3.75, image: "IMG_2461.jpeg", category: "Toy" },
+  { id: 4, name: "Puzzle", price: 4.00, image: "IMG_2462.jpeg", category: "Toy" },
+  { id: 5, name: "Teddy Bear Necklace", price: 5.50, image: "IMG_2463.jpeg", category: "Jewelry" },
+  { id: 6, name: "Dinosaur Toy", price: 6.75, image: "IMG_2464.jpeg", category: "Toy" },
+  { id: 7, name: "Superhero Figure", price: 7.99, image: "IMG_4997.jpeg", category: "Toy" }
 ];
