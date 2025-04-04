9353748de5631e463d5e3fecdbe96f2aa3d89716
SompheaReak <Thesonggaming@gmail.com>
2025-04-05 04:27:57 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,13 +1,4 @@
 const products = [
-    { id: 1, 
-    name: "HandGripper", 
-    price: 6.00} ,
-    image: "IMG_4997.jpeg"
-    },
-    { 
-    id: 2, 
-    name: "Gym Bracelet Black", 
-    price: 1.25},
-    image: "IMG_2464.jpeg"
-    }
+    { id: 1, name: "HandGripper", price: 6.00} ,image: "IMG_4997.jpeg" },
+    { id: 2, name: "Gym Bracelet Black", price: 1.25},image: "IMG_2464.jpeg" }
 ];
