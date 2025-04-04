6bfc7542a1745535e1fd77d38cc4b75bbc48bbd8
SompheaReak <Thesonggaming@gmail.com>
2025-04-05 04:29:50 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,4 +1,9 @@
 const products = [
-    { id: 1, name: "HandGripper", price: 6.00} ,image: "IMG_4997.jpeg" },
-    { id: 2, name: "Gym Bracelet Black", price: 1.25},image: "IMG_2464.jpeg" }
+  { id: 1, name: "Ninjago", price: 1.25, image: "IMG_2459.jpeg" },
+  { id: 2, name: "Toy Car", price: 2.50, image: "IMG_2460.jpeg" },
+  { id: 3, name: "Robot", price: 3.75, image: "IMG_2461.jpeg" },
+  { id: 4, name: "Puzzle", price: 4.00, image: "IMG_2462.jpeg" },
+  { id: 5, name: "Teddy Bear", price: 5.50, image: "IMG_2463.jpeg" },
+  { id: 6, name: "Dinosaur Toy", price: 6.75, image: "IMG_2464.jpeg" },
+  { id: 7, name: "Superhero Figure", price: 7.99, image: "IMG_4997.jpeg" },
 ];
