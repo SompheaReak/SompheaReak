cf28badbdab75d939aed2301e9543785d5c416dc
SompheaReak <Thesonggaming@gmail.com>
2025-04-05 14:40:33 +0700

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products = [
   { id: 2, name: "Gym Bracelet Silver", price: 1.25, image: "IMG_2463.jpeg", category: "Jewelry" },
   { id: 3, name: "Gym Bracelet Gold", price: 1.25, image: "IMG_2462.jpeg", category: "Jewelry" },
   { id: 4, name: "Gym Bracelet Black Hole", price: 1.25, image: "IMG_2461.jpeg", category: "Jewelry" },
-  { id: 5, name: "Gym Bracelet Silver Hole", price: 1.25, image: "IMG_2463.jpeg", category: "Jewelry" },
-  { id: 6, name: "Gym Bracelet Gold Hole", price: 1.25, image: "IMG_2464.jpeg", category: "Jewelry" },
+  { id: 5, name: "Gym Bracelet Silver Hole", price: 1.25, image: "IMG_2460.jpeg", category: "Jewelry" },
+  { id: 6, name: "Gym Bracelet Gold Hole", price: 1.25, image: "IMG_2459.jpeg", category: "Jewelry" },
   { id: 7, name: "Burger", price: 7, image: "IMG_4997.jpeg", category: "Jewelry" },
 ];
