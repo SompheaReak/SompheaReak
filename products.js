e471d51a69cb6baf0434f278461fa77d9c06bc3b
SompheaReak <Thesonggaming@gmail.com>
2025-04-05 14:36:53 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,9 +1,9 @@
 const products = [
   { id: 1, name: "Gym Bracelet Black", price: 1.25, image: "IMG_2464.jpeg", category: "Jewelry" },
   { id: 2, name: "Gym Bracelet Silver", price: 1.25, image: "IMG_2463.jpeg", category: "Jewelry" },
-  { id: 3, name: "Bracelet 3", price: 11, image: "IMG_2461.jpeg", category: "Jewelry" },
-  { id: 4, name: "Toy Car", price: 15, image: "IMG_2462.jpeg", category: "Toy" },
-  { id: 5, name: "Puzzle", price: 9, image: "IMG_2463.jpeg", category: "Toy" },
-  { id: 6, name: "Apple", price: 5, image: "IMG_2464.jpeg", category: "Fruit" },
-  { id: 7, name: "Burger", price: 7, image: "IMG_4997.jpeg", category: "Food" },
+  { id: 3, name: "Gym Bracelet Gold", price: 1.25, image: "IMG_2462.jpeg", category: "Jewelry" },
+  { id: 4, name: "Gym Bracelet Black Hole", price: 1.25, image: "IMG_2461.jpeg", category: "Jewelry" },
+  { id: 5, name: "Gym Bracelet Silver Hole", price: 1.25, image: "IMG_2463.jpeg", category: "Jewelry" },
+  { id: 6, name: "Gym Bracelet Gold Hole", price: 1.25, image: "IMG_2464.jpeg", category: "Jewelry" },
+  { id: 7, name: "Burger", price: 7, image: "IMG_4997.jpeg", category: "Jewelry" },
 ];
