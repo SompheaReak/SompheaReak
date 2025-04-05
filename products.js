8ae2db0a93a8bf6f81f4397f03703e9dba63cb88
SompheaReak <Thesonggaming@gmail.com>
2025-04-06 00:24:25 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,9 +1,9 @@
 const products = [
-  { id: 1, name: "Gym Bracelet Black", price: 1.25, image: "IMG_2464.jpeg", category: "Jewelry", InStock: true },
-  { id: 2, name: "Gym Bracelet Silver", price: 1.25, image: "IMG_2463.jpeg", category: "Jewelry", InStock: false },
-  { id: 3, name: "Gym Bracelet Gold", price: 1.25, image: "IMG_2462.jpeg", category: "Jewelry", InStock: true },
-  { id: 4, name: "Gym Bracelet Black Hole", price: 1.25, image: "IMG_2461.jpeg", category: "Jewelry", InStock: true },
-  { id: 5, name: "Gym Bracelet Silver Hole", price: 1.25, image: "IMG_2460.jpeg", category: "Jewelry", InStock: true },
-  { id: 6, name: "Gym Bracelet Gold Hole", price: 1.25, image: "IMG_2459.jpeg", category: "Jewelry", InStock: true },
-  { id: 7, name: "Bracelet", price: 7, image: "IMG_4997.jpeg", category: "Jewelry", InStock: false }
+  { id: 1, name: "Gym Bracelet Black", price: 1.25, image: "images/IMG_2464.jpeg", category: "Jewelry", InStock: true },
+  { id: 2, name: "Gym Bracelet Silver", price: 1.25, image: "images/IMG_2463.jpeg", category: "Jewelry", InStock: false },
+  { id: 3, name: "Gym Bracelet Gold", price: 1.25, image: "images/IMG_2462.jpeg", category: "Jewelry", InStock: true },
+  { id: 4, name: "Gym Bracelet Black Hole", price: 1.25, image: "images/IMG_2461.jpeg", category: "Jewelry", InStock: true },
+  { id: 5, name: "Gym Bracelet Silver Hole", price: 1.25, image: "images/IMG_2460.jpeg", category: "Jewelry", InStock: true },
+  { id: 6, name: "Gym Bracelet Gold Hole", price: 1.25, image: "images/IMG_2459.jpeg", category: "Jewelry", InStock: true },
+  { id: 7, name: "Bracelet", price: 7, image: "images/IMG_2458.jpeg", category: "Jewelry", InStock: false }
 ];
