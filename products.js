b184daf9d8fa6f59799e3353558e4c2bc8dc7fee
SompheaReak <Thesonggaming@gmail.com>
2025-04-05 14:32:17 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const products = [
-  { id: 1, name: "Bracelet 1", price: 11, image: "IMG_2459.jpeg", category: "Jewelry" },
-  { id: 2, name: "Bracelet 2", price: 11, image: "IMG_2460.jpeg", category: "Jewelry" },
+  { id: 1, name: "Gym Bracelet Black", price: 1.25, image: "IMG_2464.jpeg", category: "Jewelry" },
+  { id: 2, name: "Gym Bracelet Silver", price: 1.25, image: "IMG_2463.jpeg", category: "Jewelry" },
   { id: 3, name: "Bracelet 3", price: 11, image: "IMG_2461.jpeg", category: "Jewelry" },
   { id: 4, name: "Toy Car", price: 15, image: "IMG_2462.jpeg", category: "Toy" },
   { id: 5, name: "Puzzle", price: 9, image: "IMG_2463.jpeg", category: "Toy" },
